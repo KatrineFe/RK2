45143dd7a78e29f8bebf9e7f3b46ec3d08a1eb6a
Ekate <ekate@Ubuntu22.11.myguest.virtualbox.org>
2024-05-20 04:24:48 +0300

    new

--- tests/Singleton_test.cpp
+++ tests/Singleton_test.cpp
@@ -1,55 +1,48 @@
 #include <gtest/gtest.h>
 #include <iostream>
-#include <thread>
-#include <vector>
 
-// Подключение основного файла singleton.cpp
-#include "singleton.cpp"
+class Singleton {
+public:
+    static Singleton* getInstance() {
+        if (_instance == 0) {
+            _instance = new Singleton();
+        }
+        return _instance;
+    }
 
-// Тест на создание единственного экземпляра без использования потоков
-TEST(SingletonTest, SingleInstanceWithoutThreads) {
+private:
+    static Singleton* _instance;
+    Singleton() {
+        std::cout << "singleton..." << std::endl;
+    }
+};
+
+Singleton* Singleton::_instance = 0;
+
+TEST(SingletonTest, GetInstance) {
     Singleton* sgn1 = Singleton::getInstance();
     Singleton* sgn2 = Singleton::getInstance();
     ASSERT_EQ(sgn1, sgn2);
 }
 
-// Тест на потокобезопасность
-TEST(SingletonTest, ThreadSafety) {
-    std::vector<std::thread> threads;
-    for (int i = 0; i < 10; ++i) {
-        threads.emplace_back([]() {
-            Singleton::getInstance();
-        });
-    }
-
-    for (auto& thread : threads) {
-        thread.join();
-    }
-
+TEST(SingletonTest, DeleteInstance) {
     Singleton* sgn1 = Singleton::getInstance();
     Singleton* sgn2 = Singleton::getInstance();
-    ASSERT_EQ(sgn1, sgn2);
+    delete sgn1;
+    ASSERT_NO_THROW(Singleton::getInstance());
 }
 
-// Тест на создание нескольких экземпляров без обеспечения потокобезопасности
-TEST(SingletonTest, MultipleInstancesWithoutThreadSafety) {
+TEST(SingletonTest, MultipleInstances) {
     Singleton* sgn1 = Singleton::getInstance();
-    Singleton* sgn2 = nullptr;
-    Singleton* sgn3 = nullptr;
-
-    std::thread t1([&]() {
-        sgn2 = new Singleton();
-    });
-
-    std::thread t2([&]() {
-        sgn3 = new Singleton();
-    });
-
-    t1.join();
-    t2.join();
-
-    ASSERT_NE(sgn1, sgn2);
-    ASSERT_NE(sgn1, sgn3);
-    ASSERT_NE(sgn2, sgn3);
+    Singleton* sgn2 = Singleton::getInstance();
+    Singleton* sgn3 = Singleton::getInstance();
+    ASSERT_EQ(sgn1, sgn2);
+    ASSERT_EQ(sgn2, sgn3);
 }
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
 
